79d5a9afe52f718c0180d9cf249f0061518b0aaa
Umangini Naik <151855852+umangininaik15@users.noreply.github.com>
2026-09-01 20:38:19 +0530

    Add console logs for navigation debugging
    Added console log statements for debugging page navigation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,11 +139,11 @@ for (let i = 0; i < formInputs.length; i++) {
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
-
+console.log("check log");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
+console.log("check logs for loop");
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
